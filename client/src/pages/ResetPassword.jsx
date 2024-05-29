ea0fcc01e8f8c9ce33b712fcc721a495baf73c8d
WildKnuckles <jonataosacapia@gmail.com>
2024-05-29 17:09:37 +0100

    update: blog

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaEye, FaEyeSlash } from "react-icons/fa";
 
 const UserProfile = () => {
   const [newPassword, setNewPassword] = useState('');
@@ -10,11 +10,17 @@ const UserProfile = () => {
   const [error, setError] = useState('');
   const [showSuccess, setShowSuccess] = useState(false);
   const { id, token } = useParams();
+  const [showPassword, setShowPassword] = useState(false); // Estado para controlar se a senha está visível
 
   const isPasswordValid = (password) => {
     // Verifica se a senha possui pelo menos 8 caracteres, um número, uma letra maiúscula e uma minúscula
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
     return passwordRegex.test(password);
+  };
+
+  //mostrar senha
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   };
 
   const handlePasswordChange = (password) => {
@@ -69,14 +75,18 @@ const UserProfile = () => {
               <FaCheck className="success-icon" />
             )}</h2>
             {error && <p className="form-error-message">{error}</p>}
+            <div className='password-input-container'>
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               placeholder="Senha Nova"
               value={newPassword}
               onChange={(e) => handlePasswordChange(e.target.value)}
               autoFocus
             />
-            
+            <div className="password-toggle" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
+            </div>
             <button type="submit" className="btn primary">
               Atualizar
             </button>
