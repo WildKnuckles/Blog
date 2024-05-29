c0ec18c6c1a7502e658efb2fd36809d26f2f3f97
WildKnuckles <jonataosacapia@gmail.com>
2024-05-29 15:23:41 +0100

    update: blog

--- client/src/pages/ForgetPassword.jsx
+++ client/src/pages/ForgetPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { UserContext } from '../context/userContext.js';
@@ -9,6 +9,8 @@ const ForgetPassword = () => {
   });
 
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [countdown, setCountdown] = useState(0); // Estado para a contagem regressiva
   const navigate = useNavigate();
 
   const { setCurrentUser } = useContext(UserContext);
@@ -19,18 +21,31 @@ const ForgetPassword = () => {
     });
   };
 
+  useEffect(() => {
+    let timer;
+    if (countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [countdown]);
+
   const forgetPass = async (e) => {
     e.preventDefault();
     setError('');
+    setLoading(true);
     try {
-      await axios.post(`${process.env.REACT_APP_BASE_URL}/users/forget-password`, userData);   
+      await axios.post(`${process.env.REACT_APP_BASE_URL}/users/forget-password`, userData);
+      setCountdown(60); // Inicia a contagem regressiva de 60 segundos
     } catch (err) {
       if (err.response && err.response.data && err.response.data.message) {
         setError(err.response.data.message);
-         
       } else {
         setError('Erro ao tentar recuperar a senha. Por favor, tente novamente mais tarde.');
       }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -41,8 +56,8 @@ const ForgetPassword = () => {
         <form className="form login-form" onSubmit={forgetPass}>
           {error && <p className="form-error-message">{error}</p>}
           <input type="text" name="email" placeholder="Email" value={userData.email} onChange={changeInputHandler} autoFocus />
-          <button type="submit" className="btn primary">
-            Enviar
+          <button type="submit" className="btn primary" disabled={loading || countdown > 0}>
+            {loading ? 'Enviando...' : countdown > 0 ? `Aguarde ${countdown}s` : 'Enviar'}
           </button>
           <small>
             Lembra da senha? <Link to="/login">Faça login</Link>
